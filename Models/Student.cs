a63cb9ee33465ea044b99e253978c8ecac210b24
LA <lngla1810@gmail.com>
2023-09-13 22:10:56 +0700

    Add project files.

---
+++ Models/Student.cs
@@ -0,0 +1,16 @@
+﻿namespace WebApplication1.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }//Mã sinh viên
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public Branch? Branch { get; set; }//Ngành học
+        public Gender? Gender { get; set; }
+        public bool IsRegular { get; set; }//Hệ: true-cq, false-phi cq
+        public string? Address { get; set; }
+        public DateTime DateOfBorth { get; set; }//Ngày sinh
+
+    }
+}
